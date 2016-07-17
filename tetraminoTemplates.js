9ce100592e444e0761ab6d28ca660acdce53c920
Brenna Switzer <brenna.switzer@gmail.com>
2016-07-16 20:46:10 -0700

    Put tetramino templates into their own file, in preparation for rotating feature.

---
+++ tetraminoTemplates.js
@@ -0,0 +1,38 @@
+tetraminoTemplates = [
+    {
+        name: "I",
+        shape: [[0, 0], [1, 0], [2, 0], [3, 0]],
+        color: "cyan"
+    },
+    {
+        name: "J",
+        shape: [[0, 1], [1, 1], [2, 1], [2, 0]],
+        color: "blue"
+    },
+    {
+        name: "L",
+        shape: [[0, 0], [1, 0], [2, 0], [2, 1]],
+        color: "orange"
+    },
+    {
+        name: "S",
+        shape: [[1, 0], [1, 1], [0, 1], [0, 2]],
+        color: "lime"
+    },
+    {
+        name: "Z",
+        shape: [[0, 0], [0, 1], [1, 1], [1, 2]],
+        color: "red"
+    },
+    {
+        name: "T",
+        shape: [[0, 0], [0, 1], [0, 2], [1, 1]],
+        color: "purple"
+    },
+    {
+        name: "O",
+        shape: [[0, 0], [0, 1], [1, 0], [1, 1]],
+        color: "yellow"
+    }
+
+];
